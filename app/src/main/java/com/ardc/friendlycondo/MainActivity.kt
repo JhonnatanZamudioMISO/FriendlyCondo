73dfd5c33d7c27b36c68f0027feac4f6a55aecc5
Andres Lopez <am.lopezr1@uniandes.edu.co>
2022-11-09 22:22:41 -0500

    remove unused import

--- app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
+++ app/src/main/java/com/ardc/friendlycondo/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
