83eb30e3423aeeff75a3a87d7f37f5836c3eaac9
Andres Lopez <am.lopezr1@uniandes.edu.co>
2022-11-09 22:24:09 -0500

    remove unused imports

--- app/src/main/java/com/ardc/friendlycondo/features/login/LoginButton.kt
+++ app/src/main/java/com/ardc/friendlycondo/features/login/LoginButton.kt
@@ -1,17 +1,11 @@
 package com.ardc.friendlycondo.features.login
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ardc.friendlycondo.AppCanvas
 
 /**
  * ## LoginButton
